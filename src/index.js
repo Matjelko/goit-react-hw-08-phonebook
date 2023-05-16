0b619eb96c76b223fbb285f8a164d8b30fbb0317
Matjelko <matjelko11@gmail.com>
2023-05-16 14:30:22 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App/>
-      </Provider>
+      <PersistGate loading={null} persistor={persistor}>
+        <Provider store={store}>
+          <App/>
+        </Provider>
+      </PersistGate>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,8 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
-import persistReducer from "redux-persist/es/persistReducer";
 import storage from "redux-persist/lib/storage"
-import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import { persistStore, persistReducer } from "redux-persist";
 import { authReducer } from "./auth/slice";
 
 const authPersistConfig = {
@@ -12,17 +11,15 @@ const authPersistConfig = {
     whitelist: ["token"]
 }
 
-const middleware = {
-    serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-    }
-}
-
 export const store = configureStore({
     reducer: {
         auth: persistReducer(authPersistConfig, authReducer),
         contacts: contactsReducer,
         filter: filterReducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware)
-})
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
+});
+
+export const persistor = persistStore(store)
